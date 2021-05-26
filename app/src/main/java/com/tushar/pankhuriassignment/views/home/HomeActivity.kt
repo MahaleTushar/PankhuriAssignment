0caa515ddc9cef83a694e434791033f3e8fadcd0
Tushar Mahale <tmahale05@gmail.com>
2021-05-26 23:31:56 +0530

    Required drawable icons added

--- app/src/main/java/com/tushar/pankhuriassignment/views/home/HomeActivity.kt
+++ app/src/main/java/com/tushar/pankhuriassignment/views/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.tushar.pankhuriassignment
+package com.tushar.pankhuriassignment.views.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tushar.pankhuriassignment.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }

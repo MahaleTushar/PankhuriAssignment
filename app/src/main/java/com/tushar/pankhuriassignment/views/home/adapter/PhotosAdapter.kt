764981526109524f31dcb597928289632cc1dd25
Tushar Mahale <tmahale05@gmail.com>
2021-05-27 14:14:37 +0530

    [PhotoFragment] PhotoFragment data displayed

--- app/src/main/java/com/tushar/pankhuriassignment/views/home/adapter/PhotosAdapter.kt
+++ app/src/main/java/com/tushar/pankhuriassignment/views/home/adapter/PhotosAdapter.kt
@@ -4,16 +4,16 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.tushar.pankhuriassignment.R
-import com.tushar.pankhuriassignment.views.home.ImageViewHolder
+import com.tushar.pankhuriassignment.views.home.viewholder.PhotoViewHolder
 
-class PhotosAdapter : RecyclerView.Adapter<ImageViewHolder>() {
+class PhotosAdapter : RecyclerView.Adapter<PhotoViewHolder>() {
     val imageUrl = ArrayList<String>()
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.rv_card, parent, false)
-        return ImageViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.photo_card, parent, false)
+        return PhotoViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
         holder.onBind(imageUrl[position])
     }
 
--- app/src/main/java/com/tushar/pankhuriassignment/views/home/viewholder/PhotoViewHolder.kt
+++ app/src/main/java/com/tushar/pankhuriassignment/views/home/viewholder/PhotoViewHolder.kt
@@ -1,12 +1,12 @@
-package com.tushar.pankhuriassignment.views.home
+package com.tushar.pankhuriassignment.views.home.viewholder
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import kotlinx.android.synthetic.main.rv_card.view.*
+import kotlinx.android.synthetic.main.photo_card.view.*
 
-class ImageViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+class PhotoViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
     fun onBind(imageUrl: String) {
         Glide.with(view).load(imageUrl)
             .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
